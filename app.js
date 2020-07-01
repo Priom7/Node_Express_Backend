459c1c30be4fe87c9a48966354d8f47ee029bc78
Md. Sharif Alam <priom7197@gmail.com>
2020-07-01 20:41:36 +0600

    edited port

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-soxqi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((error) => {
     console.log(error);
